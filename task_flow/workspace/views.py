1e23693a8a155fac9ac709f198d0dc4be4829c98
juuxi <dimashopyrev@mail.ru>
2026-08-25 18:00:28 +0300

    impl permissions for tasks and projects

--- task_flow/workspace/views.py
+++ task_flow/workspace/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+
+from django.shortcuts import get_object_or_404
+
 from .models import Project, Task, Workspace
 from .serializers import ProjectSerializer, TaskSerializer, WorkspaceSerializer
+from .permissions import IsWorkspaceMember
 
 
 class WorkspaceViewSet(viewsets.ModelViewSet):
@@ -13,10 +17,20 @@ class WorkspaceViewSet(viewsets.ModelViewSet):
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsWorkspaceMember]
+
+    def get_object(self):
+        obj = get_object_or_404(self.get_queryset(), pk=self.kwargs["pk"])
+        self.check_object_permissions(self.request, obj.workspace)
+        return obj
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsWorkspaceMember]
+
+    def get_object(self):
+        obj = get_object_or_404(self.get_queryset(), pk=self.kwargs["pk"])
+        self.check_object_permissions(self.request, obj.project.workspace)
+        return obj
